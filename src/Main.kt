0e3ea744762df917f4fb3a7f8d9208ccfb39d40e
Utree <yuukis322+github@gmail.com>
2018-10-19 15:47:35 +0900

    グリッドワールドの構造を変更

--- src/Main.kt
+++ src/Main.kt
@@ -49,29 +49,7 @@ data class Position(var x: Int, var y: Int)
  * @property cells
  * グリッドワールドのマップチップ
  */
-data class World(var cells: Array<Array<Cell>>) {
-    // Javaデコンパイルすると配列の比較ができなくなるので対処
-    override fun equals(other: Any?): Boolean {
-        if(this === other) return true
-        if(javaClass != other?.javaClass) return false
-        other as World
-        if(!Arrays.equals(cells, other.cells)) return false
-        return true
-    }
-    // Javaデコンパイルすると同じ値でもhash値が変わってしまうので対処
-    override fun hashCode(): Int {
-        return Arrays.hashCode(cells)
-    }
-    // デバッグ用
-    override fun toString(): String {
-        for(i in cells) {
-            for(j in i) {
-                print("Cell(${j.left}, ${j.up}, ${j.down}, ${j.right})")
-            }
-        }
-        return ""
-    }
-}
+data class World(var cells: MutableMap<Position, Cell>)
 
 /**
  * エージェント
@@ -128,35 +106,42 @@ fun main(args: Array<String>) {
 
     /** エージェント間で報酬を共有するかのフラグ */
     // Todo: 標準入力からデータをもらう
-    var shareRewardsFlag = true
+    var shareRewards = true
 
     /** 学習を繰り返すたびにエージェントの位置を変更するかのフラグ */
     // Todo: 標準入力からデータをもらう
-    var changeAgentPositionFlag = true
+    var changeAgentPosition = true
 
-    /** グリッドワールドをつくる */
-    var initWorld = World(arrayOf(emptyArray()))
+    /** グリッドワールドを生成 */
     // iは縦軸, jは横軸
+    var initWorld = World(mutableMapOf())
     for(i in 0 until grids) {
-        var tmpCell = emptyArray<Cell>()
-        for(j in 0 until grids) {
+        for (j in 0 until grids) {
             // up
-            val up: Int = if(i > 0) 0 else -1
+            val up: Int = if (i > 0) 0 else -1
             // down
-            val down: Int = if(i < grids-1) 0 else -1
+            val down: Int = if (i < grids - 1) 0 else -1
             // right
-            val right: Int = if(j < grids-1) 0 else -1
+            val right: Int = if (j < grids - 1) 0 else -1
             // left
-            val left: Int = if(j > 0) 0 else -1
+            val left: Int = if (j > 0) 0 else -1
 
-            tmpCell += Cell(left, up, down, right)
+            initWorld.cells[Position(j, i)] = Cell(left, up, down, right)
         }
-        initWorld.cells += tmpCell
     }
 
-    /** agentのインスタンス */
+    // 表示
+    for(i in 0 until grids)
+        for(j in 0 until grids) {
+            println("${Position(j, i)}: ")
+            println("${initWorld.cells[Position(j, i)]} \n")
+        }
+
+    /** agentのインスタンスを生成 */
     var agent1 = Agent(initWorld, arrayOf(agentInitPosition1))
     var agent2 = Agent(initWorld, arrayOf(agentInitPosition2))
+
+    //
 
     print("Hello Kotlin")
 }
